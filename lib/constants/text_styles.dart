8675b9f5855a8e7dd8de1144f2c84d8c3c885579
ahmadsultani <ahmadsultanidayanullah@gmail.com>
2024-07-31 15:38:39 +0800

    feat: text styles placeholder

---
+++ lib/constants/text_styles.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class TextStyles {
+  static const TextStyle title = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle subtitle = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle body = TextStyle(
+    fontSize: 14,
+  );
+
+  static const TextStyle caption = TextStyle(
+    fontSize: 12,
+  );
+
+  static const TextStyle button = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+  );
+}

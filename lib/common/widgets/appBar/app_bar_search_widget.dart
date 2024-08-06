8d80d76a33f63b29bd9eb246745b74478d357e8f
reynaldabnerr <reynald030685@gmail.com>
2024-08-06 15:43:28 +0800

    fix: app bar menu widget

---
+++ lib/common/widgets/appBar/app_bar_search_widget.dart
@@ -0,0 +1,75 @@
+import 'package:alumni_hub_ft_uh/common/widgets/appBar/app_bar_menu_widget.dart';
+import 'package:flutter/material.dart';
+
+class AppBarSearchWidget extends StatelessWidget implements PreferredSizeWidget {
+  const AppBarSearchWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      leading: IconButton(
+        icon: const Icon(Icons.menu, color: Colors.black),
+        onPressed: () {
+          // Navigate to AppBarMenuWidget with slide animation
+          Navigator.push(
+            context,
+            PageRouteBuilder(
+              pageBuilder: (context, animation, secondaryAnimation) {
+                return const AppBarMenuWidget();
+              },
+              transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                const begin = Offset(-1.0, 0.0); // Start from the left
+                const end = Offset.zero; // End at the center
+                const curve = Curves.easeInOut;
+
+                var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+                var offsetAnimation = animation.drive(tween);
+
+                return SlideTransition(position: offsetAnimation, child: child);
+              },
+            ),
+          );
+        },
+      ),
+      title: Container(
+        height: 35,
+        width: 300,
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.black, width: 1.0), // Outline color and width
+          borderRadius: BorderRadius.circular(40.0), // Rounded corners
+        ),
+        child: Row(
+          children: [
+            const Icon(Icons.search, color: Colors.black),
+            Expanded(
+              child: TextField(
+                autofocus: true, // Automatically focus when the AppBar is displayed
+                decoration: InputDecoration(
+                  hintText: 'Search',
+                  hintStyle: Theme.of(context).textTheme.bodySmall?.copyWith(color: Colors.black54),
+                  border: InputBorder.none,
+                  contentPadding: const EdgeInsets.symmetric(vertical: 14, horizontal: 8.0), // Adjust height and horizontal padding
+                ),
+                style: Theme.of(context).textTheme.bodyMedium?.copyWith(color: Colors.black),
+              ),
+            ),
+          ],
+        ),
+      ),
+      actions: const [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage('https://via.placeholder.com/150'),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
--- lib/features/news/news_screen.dart
+++ lib/features/news/news_screen.dart
@@ -1,3 +1,4 @@
+import 'package:alumni_hub_ft_uh/common/widgets/appBar/app_bar_search_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:alumni_hub_ft_uh/common/widgets/appBar/app_bar_widget.dart';
 import 'package:alumni_hub_ft_uh/common/widgets/button/button_filter_widget.dart';
@@ -105,7 +106,7 @@ class _NewsScreenState extends State<NewsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const AppBarWidget(),
+      appBar: const AppBarSearchWidget(),
       bottomNavigationBar: const BottomBarWidget(currentIndex: 1),
       body: SafeArea(
         child: SingleChildScrollView(

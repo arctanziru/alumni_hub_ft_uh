93d21c1b540647212c93199c886746157b19eac6
ahmadsultani <ahmadsultanidayanullah@gmail.com>
2024-08-08 05:42:11 +0800

    feat: default page builder simple animation

--- lib/route.dart
+++ lib/route.dart
@@ -50,17 +50,7 @@ PageRoute _buildPageRoute(Widget child) {
   return PageRouteBuilder(
     pageBuilder: (context, animation, secondaryAnimation) => child,
     transitionsBuilder: (context, animation, secondaryAnimation, child) {
-      const begin = Offset(1.0, 0.0);
-      const end = Offset.zero;
-      const curve = Curves.ease;
-
-      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-      var offsetAnimation = animation.drive(tween);
-
-      return SlideTransition(
-        position: offsetAnimation,
-        child: child,
-      );
+      return child;
     },
   );
 }
